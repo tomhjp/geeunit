cdd621dea04ce40a1b65cd54c24bb34ca7a6b69c
Tom <tp327@cam.ac.uk>
2013-05-21 17:52:21 +0100

    Updated scanner class interface

--- src/scanner.h
+++ src/scanner.h
@@ -6,8 +6,8 @@ class scanner_t
     unsigned int line, col;
 public:
     scanner_t();
+    symbol_t nextSymbol(void);
     void getPosition(int &line, int &col, bool &ok);
-    
 };
 
 #endif  /* scanner_h */
