df449e687ce47a0dad75833ec03bd344832ceb83
Tom <tp327@cam.ac.uk>
2013-05-22 13:38:19 +0100

    Updated interface and continued work

--- src/scanner.cc
+++ src/scanner.cc
@@ -4,3 +4,23 @@
 
 #include "scanner.h"
 
+scanner_t::scanner_t(names *namesObj, const char *defname)
+{
+    namesObj = namesObj;
+    file = defname;
+}
+
+scanner_t::~scanner_t()
+{
+    
+}
+
+scanner_t::void nextSymbol(symbol_t &symbol, name_t &name, int &num)
+{
+    
+}
+
+scanner_t::void getPosition(int &line, int &col, bool &ok)
+{
+    
+}
--- src/scanner.h
+++ src/scanner.h
@@ -4,10 +4,25 @@
 class scanner_t
 {
     unsigned int line, col;
+    char *file;
+    names *namesObj;
 public:
-    scanner_t();
-    symbol_t nextSymbol(void);
+    scanner_t(names *namesObj, const char *defname);
+      /* Constructor for the scanner class
+       * Arg1: pointer to an instance of names class
+       * Arg2: pointer to the definition file */
+    ~scanner_t();
+      /* Destructor for the scanner class */
+    void nextSymbol(symbol_t &symbol, name_t &name, int &num);
+      /* Return the next symbol in the definition file 
+       * Arg1: Type of symbol, e.g. semicol, namesym numsym etc.
+       * Arg2: Returns the name if the symbol has one
+       * Arg3: Returns the number if the symbol is a number */
     void getPosition(int &line, int &col, bool &ok);
+      /* Return the position of the last symbol the scanner scanned 
+       * Arg1: line number
+       * Arg2: column number
+       * Arg3: flag to check function executed correctly */
 };
 
 #endif  /* scanner_h */
