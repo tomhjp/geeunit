01634858c22dda74c879ef43767bf7bf6726d83e
Tom <tp327@cam.ac.uk>
2013-05-28 21:19:52 +0100

    Increased max monitors to 50, other small changes including adding more devices to guitest

--- src/monitor.h
+++ src/monitor.h
@@ -5,7 +5,7 @@
 #include "network.h"
 #include "devices.h"
 
-const int maxmonitors = 10;      /* max number of monitor points */
+const int maxmonitors = 50;      /* max number of monitor points */
 const int maxcycles = 100;        /* max number of cycles per run */
 
 struct moninfo {
